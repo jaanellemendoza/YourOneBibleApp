904766cf7fa69cb549513e473dcf847c55a8bad0
jaanellemendoza <103169838+jaanellemendoza@users.noreply.github.com>
2022-07-07 13:06:25 +0800

    bible app

---
+++ components/screens/homeScreen.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import LottieView from 'lottie-react-native';
+
+const HomeScreen = () => {
+  return (
+    
+    <View style={{ flex: 1, backgroundColor: "grey", alignItems: "center" }}>
+    <Text style={{ fontSize: 35, width: '100%', marginTop: 50, marginLeft: 100, }}>Bible Home Screen</Text>
+  </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
